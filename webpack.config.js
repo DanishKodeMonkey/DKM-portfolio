4a52f38134a51fa9231d4ea357635c256361fe25
danishkodemonkey <humberjini@hotmail.com>
2024-04-02 10:36:58 +0200

    BREAKING: dev enviroment setup

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	devtool: 'inline-source-map',
+	devServer: {
+		static: './dist',
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			title: "danishKodeMonkey's portfolio",
+			template: './src/index.html',
+		}),
+	],
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
+}
